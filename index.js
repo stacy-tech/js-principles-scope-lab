9907030bb03c51ce3fc64cd1de130735bc6c2878
stacy <stacydn23@gmail.com>
2021-05-17 00:47:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// declares a variable customerName using var 
+var customerName = 'bob'
+
+// a constant in global scope 
+const leastFavoriteCustomer = 'mark'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john'
+}
